b3a4a0c91bb4a6f0364ec43757211a4948a0b105
AnotherTest <ali.mpfard@gmail.com>
2021-04-13 19:12:25 +0430

    LibC: Sneak in the actual definition of assert() as _do_assert()
    This is a hack to allow customising the definition of assert() without
    losing the original definition.

--- Userland/Libraries/LibC/assert.h
+++ Userland/Libraries/LibC/assert.h
@@ -34,14 +34,16 @@ __BEGIN_DECLS
 __attribute__((noreturn)) void __assertion_failed(const char* msg);
 #    define __stringify_helper(x) #    x
 #    define __stringify(x) __stringify_helper(x)
-#    define assert(expr)                                                           \
+#    define _do_assert(expr)                                                       \
         do {                                                                       \
             if (__builtin_expect(!(expr), 0))                                      \
                 __assertion_failed(#expr "\n" __FILE__ ":" __stringify(__LINE__)); \
         } while (0)
+#    define assert(x) _do_assert(x)
 #    define VERIFY_NOT_REACHED() assert(false)
 #else
-#    define assert(expr) ((void)(0))
+#    define _do_assert(expr) ((void)(0))
+#    define assert(x) _do_assert(x)
 #    define VERIFY_NOT_REACHED() CRASH()
 #endif
 
